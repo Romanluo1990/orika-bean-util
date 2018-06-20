93ac78bbf777c20c84e5f4e97b2c68df6bd35924
roman.luo <530827804@qq.com>
2018-06-20 09:26:27 +0800

    重组结构

--- orika-core/src/test/java/roman/common/util/bean/BeanUtilsTest.java
+++ orika-core/src/test/java/roman/common/util/bean/BeanUtilsTest.java
@@ -14,13 +14,5 @@ public class BeanUtilsTest {
 
         UserA userA = BeanUtils.map(user, UserA.class);
         System.out.println(userA.toString());
-//
-//        MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
-//        mapperFactory.classMap(User.class, UserA.class)
-//                .field("name","nameB")
-//                .byDefault()
-//                .register();
-
-//        System.out.println(mapperFactory.getMapperFacade().map(user,UserA.class));
     }
 }
